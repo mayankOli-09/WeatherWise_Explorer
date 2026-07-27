2952054bdba74e53c012fc4a952823893e207cd8
mayankOli-09 <240211193@geu.ac.in>
2026-07-27 21:09:46 +0530

    Add files via upload

---
+++ JSONArray.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class JSONArray {
+
+    public int length() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'length'");
+    }
+
+    public Object getString(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getString'");
+    }
+
+    public int getInt(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInt'");
+    }
+
+}
