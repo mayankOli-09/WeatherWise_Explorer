3c7154426ec3c89c7a52a8a61536b47ccf6f3371
mayankOli-09 <240211193@geu.ac.in>
2026-07-28 18:27:29 +0530

    Add files via upload

---
+++ JSONObject.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class JSONObject {
+
+    public JSONObject getJSONObject(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getJSONObject'");
+    }
+
+    public int getInt(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInt'");
+    }
+
+    public double getDouble(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDouble'");
+    }
+
+    public JSONArray getJSONArray(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getJSONArray'");
+    }
+
+}
